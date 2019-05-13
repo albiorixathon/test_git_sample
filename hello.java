1f3bed41af5c60dba197bb81e7f8b303be81c33c
albiorixathon <albiorixathon@users.noreply.github.com>
2019-05-13 14:52:39 -0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 // Hello world
 class Solution {
 	public static void main(String args[]) {
-		System.out.println("Hello World (test 001) !");
+		System.out.println("Hello World (test 002) !");
 	}
 }
